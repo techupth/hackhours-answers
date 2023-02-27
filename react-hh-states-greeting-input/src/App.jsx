286dd8b70bb00d94575139e811cd0775f1eec580
Mind Sathirasrindh <109655871+mydjolie@users.noreply.github.com>
2023-02-27 12:00:26 +0700

    feat:add ex-2

---
+++ react-hh-states-greeting-input/src/App.jsx
@@ -0,0 +1,36 @@
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [message, setMessage] = useState("");
+  const [greetingTextInput, setGreetingTextInput] = useState("");
+
+  const handleTextInputChange = (e) => {
+    setGreetingTextInput(e.target.value);
+  };
+
+  const handleAddClick = () => {
+    setMessage(greetingTextInput);
+  };
+
+  return (
+    <div className="App">
+      <div className="greeting-container">{message}</div>
+      <div className="input-container">
+        <label htmlFor="greeting-message">New Greeting Message</label>
+        <input
+          id="greeting-message"
+          type="text"
+          onChange={handleTextInputChange}
+          value={greetingTextInput}
+        />
+      </div>
+
+      <div className="buttons">
+        <button onClick={handleAddClick}>Update text</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
