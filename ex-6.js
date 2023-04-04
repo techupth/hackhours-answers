0e18acc68ffdd9e4cca93a548eae5ea9bab5d134
Mind Sathirasrindh <109655871+mydjolie@users.noreply.github.com>
2023-04-04 14:42:22 +0700

    fix:add ex6-7

---
+++ ex-6.js
@@ -0,0 +1,5 @@
+//ex-6
+let height = undefined;
+let result = height ?? "Height doesn't defined";
+
+console.log(result); //  Result ควรจะได้ออกมาเป็น “Height doesn't defined”
---
+++ ex-7.js
@@ -0,0 +1,20 @@
+//ex-7
+const employee1 = {
+  name: "John",
+  age: 20,
+};
+
+const result1 = employee1.scores?.english;
+
+const employee2 = {
+  name: "A",
+  age: 50,
+  scores: {
+    math: 40,
+  },
+};
+
+const result2 = employee2.scores?.english ?? "English score does not defined";
+
+console.log(result1);
+console.log(result2);
