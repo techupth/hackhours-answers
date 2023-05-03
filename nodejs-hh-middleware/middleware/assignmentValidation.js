4a8ee3964a472fd7b116c0815709a1d1b51e1cfd
Mind Sathirasrindh <109655871+mydjolie@users.noreply.github.com>
2023-05-03 17:12:36 +0700

    fix:check spelling

--- nodejs-hh-middleware/middleware/assignmentValidation.js
+++ nodejs-hh-middleware/middleware/assignmentValidation.js
@@ -1,16 +1,22 @@
 function validateAssignmentData(req, res, next) {
   const body = req.body;
-  
+
   if (body.title.length > 35) {
-    return res.status(401).json({ message: "Title must not be over 35 characters" });
+    return res
+      .status(400)
+      .json({ message: "Title must not be over 35 characters" });
   }
-  
+
   if (body.description > 150) {
-    return res.status(401).json({ message: "Description must not exceed 150 characters" });
+    return res
+      .status(400)
+      .json({ message: "Description must not exceed 150 characters" });
   }
-  
-  if (!Array.isArray(body.catergories) || body.catergories.length < 1) {
-    return res.status(401).json({ message: "Categories must be an array with at least 1 value" });
+
+  if (!Array.isArray(body.categories) || body.categories.length < 1) {
+    return res
+      .status(400)
+      .json({ message: "Categories must be an array with at least 1 value" });
   }
 
   next();
