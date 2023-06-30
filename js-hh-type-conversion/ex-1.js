35f664d68efab4428158d93425927e5f6f7f56a7
knotnapat <napat.wongchr@gmail.com>
2023-06-30 23:07:08 +0700

    feat: add ans

---
+++ js-hh-type-conversion/ex-1.js
@@ -0,0 +1,8 @@
+let accountBalance = 400;
+let depositAmountFromUser = "10";
+
+accountBalance = accountBalance + Number(depositAmountFromUser);
+
+let accountBalanceMessage = "Your account balance is " + accountBalance;
+
+console.log(accountBalanceMessage);
