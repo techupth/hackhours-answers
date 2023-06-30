74a5f55912819d76955bf1afc56b2ccc3a2224fb
knotnapat <napat.wongchr@gmail.com>
2023-06-30 22:52:10 +0700

    feat: add ans

---
+++ js-hh-variables-values/ex-1and-2.js
@@ -0,0 +1,13 @@
+//Exercise #1: Variable Creation Start coding here
+let firstName = "Karter";
+let lastName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+
+const restaurantName = "Chef's Table by Chef James";
+const totalPrice = "15000";
+const restaurantNumber = "1500";
+
+//Exercise #2: Variable Reassignment Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+totalPrice = 5000;
+restaurantNumber = 2500;
---
+++ js-hh-variables-values/ex-3.js
@@ -0,0 +1,6 @@
+let celcius = 30;
+
+// Exercise #3: Celsius to Fahrenheit
+// Start coding here;
+
+let fahrenheit = celcius * 1.8 + 32;
