1f41a604b685fa44d0165b0fdaae3d2e1146dd40
knotnapat <napat.wongchr@gmail.com>
2023-07-06 10:01:29 +0700

    fix: answers

--- js-hh-computational-thinking/ex-1.js
+++ js-hh-computational-thinking/ex-1.js
@@ -9,8 +9,8 @@ function addNumber(numbers, amount) {
     newNumbers.push(numbers[i] + amount);
   }
 
-  return newNumber;
+  return newNumbers;
 }
 
-addNumber(numbers, 10); // Output: [20, 30, 40]
-addNumber(anotherNumbers, 2); // Output: [4, 6, 8]
+console.log(addNumber(numbers, 10)); // Output: [20, 30, 40]
+console.log(addNumber(anotherNumbers, 2)); // Output: [4, 6, 8]
--- js-hh-computational-thinking/ex-2.js
+++ js-hh-computational-thinking/ex-2.js
@@ -1,15 +1,15 @@
 //Exercise 2
-function countVowels(str) {
+function countVowels(words) {
   let vowels = "aeiou";
   let count = 0;
 
-  for (let i = 0; i < str.length; i++) {
-    if (vowels.includes(str[i].toLowerCase())) {
+  for (let i = 0; i < words.length; i++) {
+    if (vowels.includes(words[i].toLowerCase())) {
       count++;
     }
   }
   return count;
 }
 
-console.log(countVowels("Hello World"));
-// Output: 3
+console.log(countVowels("Hello World")); // Output: 3
+console.log(countVowels("TechUp")); // Output: 2
