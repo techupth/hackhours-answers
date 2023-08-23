2c5badf1661b11decf53e2c3a8823918434eb9ca
jettapat <jettapat@techupth.com>
2023-08-23 14:50:37 +0700

    fix: starter code

--- js-hh-callbacks-and-hofs/ex-2.js
+++ js-hh-callbacks-and-hofs/ex-2.js
@@ -15,7 +15,7 @@ function checkGradeOver70(score) {
   return score > 70;
 }
 
-const studentScoresRoom1 = [12, 40, 67, 80, 100, 15, 40, 12, 40, 67];
+const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
 const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
 const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
