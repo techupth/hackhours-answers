3f6057ebd340c44db363ce82859f5dddb6376583
Napat <napat.wongchr@gmail.com>
2023-12-20 17:02:19 +0700

    fix: update answer

---
+++ react-hh-displaying-lists/src/components/MovieList.jsx
@@ -0,0 +1,87 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import { movies } from "../data/movies";
+
+function MoviesList() {
+  return (
+    <div>
+      {movies.map((item, index) => {
+        return (
+          <div
+            key={index}
+            className="movie-card"
+            css={css`
+              display: flex;
+              background-color: #ffffff;
+              height: 250px;
+              border-radius: 10px;
+              padding: 18px 14px 18px 14px;
+              box-shadow: 0px 4px 40px rgba(145, 124, 124, 0.25);
+              margin-bottom: 80px;
+            `}
+          >
+            <div className="movie-preview">
+              <img
+                src={item.image}
+                css={css`
+                  width: 102px;
+                  height: 100px;
+                  object-fit: cover;
+                  border-radius: 10px;
+                `}
+              ></img>
+            </div>
+            <div
+              className="movie-description"
+              css={css`
+                margin-left: 20px;
+                font-size: 20px;
+                display: flex;
+                flex-direction: column;
+                justify-content: space-between;
+              `}
+            >
+              <div className="movie-title">Title: {item.title}</div>
+              <div className="movie-year">Year: {item.year}</div>
+              <div className="movie-runtime">Runtime: {item.runtime}</div>
+              <div
+                className="movie-genres"
+                css={css`
+                  display: flex;
+                `}
+              >
+                Genres:
+                {item.genres.map((genre, i) => {
+                  return (
+                    <div
+                      key={i}
+                      className="movie-genres-bubble"
+                      css={css`
+                        background-color: #eaac99;
+                        border-radius: 10px;
+                        font-size: 16px;
+                        padding-left: 10px;
+                        padding-right: 10px;
+                        margin-left: 10px;
+                        margin-top: 5px;
+                      `}
+                    >
+                      {genre}
+                    </div>
+                  );
+                })}
+              </div>
+              <div className="movie-imdb-ratings">
+                IMDB Ratings: {item.imdbRating}
+              </div>
+              <div className="movie-imdb-imdbVotes">
+                IMDB Votes: {item.imdbVotes}
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+export default MoviesList;
